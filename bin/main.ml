32cd21eb9379e5ff5a764af12d90c7911dadb221
Ruth Taddesse <ryt5@cornell.edu>
2024-10-30 16:11:52 -0400

    added details to mli documentation + made the most basic outline for the driver

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,12 @@
-let () = print_endline "We love 3110! :)"
+open Project3110
+
+let main () =
+  print_endline "Welcome to Monopoly Deal!\n";
+  let player1 = Player.init_player "player 1" in
+  let player2 = Player.init_player "player 2" in
+  let players = [ player1; player2 ] in
+  let _ = Game_state.init_game players in
+  print_endline "Thanks for playing!"
+;;
+
+main ()
