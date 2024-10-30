d3fc4152aeedafeb914d41428acf0fa75014c7d7
Temi <ta375@cornell.edu>
2024-10-30 16:45:27 -0400

    Ad get users card chocie in terminal

--- bin/main.ml
+++ bin/main.ml
@@ -3,18 +3,6 @@ open Player
 open Game_state
 open Deck
 
-(* 
-
-   print_hand
-
-   get_card_choice
-
-   play_turn
-
-   game_loop
-
-   () - continously calls next turn until *)
-
 let rec print_hand cards =
   match cards with
   | [] -> ()
@@ -26,7 +14,19 @@ let rec print_hand cards =
       | Action name -> Printf.printf "Action card: %s\n" name);
       print_hand rest
 
-open Project3110
+let get_card_choice hand =
+  print_endline "\nYour hand:";
+  List.iteri
+    (fun i card ->
+      Printf.printf "%d: " i;
+      match card with
+      | Money amount -> Printf.printf "Money card: $%dM\n" amount
+      | Property (color, name) ->
+          Printf.printf "Property card: %s - %s\n" color name
+      | Action name -> Printf.printf "Action card: %s\n" name)
+    hand;
+  print_string "Choose a card to play (enter number) or -1 to skip: ";
+  read_int ()
 
 let main () =
   print_endline "Welcome to Monopoly Deal!\n";
@@ -38,3 +38,15 @@ let main () =
 ;;
 
 main ()
+
+(* 
+
+   print_hand
+
+   get_card_choice
+
+   play_turn
+
+   game_loop
+
+   () - continously calls next turn until *)
