7f0e015d9fa1607f1a344b74a6e9a608841f9a52
Celeste Naughton <can82@cornell.edu>
2024-12-09 10:36:37 -0500

    finish testing deck.mli

--- test/test_project3110.ml
+++ test/test_project3110.ml
@@ -3,11 +3,14 @@ open Project3110.Deck
 
 let test_deck = Project3110.Deck.shuffle_deck (Project3110.Deck.init_deck ())
 
+let test_card_action test_name input output =
+  test_name >:: fun _ -> assert_equal input output
+
 let tests =
   "test suite"
   >::: [
          ("Trivial test" >:: fun _ -> assert_equal 0 0);
-         ("Deck test" >:: fun _ -> assert_equal (List.length test_deck) 110);
+         ("Deck test" >:: fun _ -> assert_equal (List.length test_deck) 89);
        ]
 
 let _ = run_test_tt_main tests
