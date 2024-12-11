4a00dc261e8615fe05c4ca61a31a6737b58d23b2
Temi <ta375@cornell.edu>
2024-12-11 17:12:26 -0500

    Working versio of merged test suite

--- test/test_project3110.ml
+++ test/test_project3110.ml
@@ -27,10 +27,10 @@ let rec add_property player deck =
   | [] -> player
   | card :: rest ->
       let updated_player = Project3110.Player.add_property player card in
-      add_properties updated_player rest
+      add_property updated_player rest
 
 let player1 =
-  add_properties player1_init
+  add_property player1_init
     [
       ("Light Blue", "Oriental Avenue");
       ("Light Blue", "Vermont Avenue");
@@ -56,22 +56,8 @@ let player1_remove_sets =
 let player1 = Project3110.Player.remove_from_bank player1 8
 let player2 = Project3110.Player.bank_money player1 6
 
-(************************* Beginning of Test Suite ****************************)
-let create_test test_name input output =
-(* Player hand can have no more than 8 cards *)
-
-(* Helper functions *)
-
-let init_test_players () = (init_player "p1", init_player "p2")
-
-let add_test_properties player =
-  add_properties player
-    [
-      ("Brown", "Mediterranean Avenue");
-      ("Light Blue", "Oriental Avenue");
-      ("Light Blue", "Vermont Avenue");
-      ("Light Blue", "Connecticut Avenue");
-    ]
+(*************** Helpers for Testing CardAction.mli
+  *******************************)
 
 let test_player test_name input output =
   test_name >:: fun _ -> assert_equal input output
@@ -81,7 +67,22 @@ let test_has_property test_name player property expected =
     (List.mem property (Project3110.Player.get_properties player))
     expected
 
-(* Card Action Tests *)
+let init_test_players () = (init_player "p1", init_player "p2")
+
+let add_test_properties player =
+  add_property player
+    [
+      ("Brown", "Mediterranean Avenue");
+      ("Light Blue", "Oriental Avenue");
+      ("Light Blue", "Vermont Avenue");
+      ("Light Blue", "Connecticut Avenue");
+    ]
+
+(************************* Beginning of Test Suite ****************************)
+let create_test test_name input output =
+  test_name >:: fun _ -> assert_equal input output
+
+(************************* Card Action Tests ****************************)
 
 let card_action_tests =
   let p1, p2 = init_test_players () in
@@ -114,79 +115,79 @@ let card_action_tests =
       true;
   ]
 
-(* Game State Tests *)
+(************************* Game State Tests ****************************)
 
 let tests =
-[
-         (******************** Deck.mli tests ********************************)
-         ("Deck test" >:: fun _ -> assert_equal (List.length test_deck) 89);
-         create_test "Testing - draw_card" (List.length new_deck) 82;
-         (******************** Player.mli tests ********************************)
-         (******************** init_player tests *****************************)
-         create_test "Testing Player - init name"
-           (Project3110.Player.get_name player1_init)
-           "player1";
-         create_test "Testing Player - init properties"
-           (Project3110.Player.get_properties player1_init)
-           [];
-         create_test "Testing Player - init bank: 0"
-           (Project3110.Player.get_bank player1_init)
-           0;
-         create_test "Testing Player - init deck: empty list"
-           (Project3110.Player.get_hand player1_init)
-           [];
-         (********************* add/remove_from_hand tests *******************)
-         create_test "Testing Player - add cards to hand "
-           (List.rev (Project3110.Player.get_hand player1))
-           [
-             Property ("Railroad", "Reading Railroad");
-             Property ("Railroad", "Pennsylvania Railroad");
-             Money 4;
-             Money 1;
-             Action "Debt Collector";
-           ];
-         create_test "Testing Player - remove card from empty hand"
-           (Project3110.Player.remove_from_hand player1_init (Money 5))
-           player1_init;
-         create_test "Testing Player - Remove card from hand that isn't there"
-           (List.rev
-              (Project3110.Player.get_hand
-                 (Project3110.Player.remove_from_hand player1 (Money 5))))
-           [
-             Property ("Railroad", "Reading Railroad");
-             Property ("Railroad", "Pennsylvania Railroad");
-             Money 4;
-             Money 1;
-             Action "Debt Collector";
-           ];
-         create_test "Testing Player - removing a card from a player"
-           (List.rev
-              (Project3110.Player.get_hand
-                 (Project3110.Player.remove_from_hand player1 (Money 4))))
-           [
-             Property ("Railroad", "Reading Railroad");
-             Property ("Railroad", "Pennsylvania Railroad");
-             Money 1;
-             Action "Debt Collector";
-           ];
-         (******** add/remove properites/ get_property_sets test *************)
-         create_test "Testing Player - 1 full set in properties"
-           (Project3110.Player.get_property_sets player1)
-           1;
-         (***************** bank_money/remove_from_bank tests *****************)
-         create_test "Testing Player - Removing money from 0 bank "
-           (Project3110.Player.get_bank player1)
-           0;
-         create_test "Testing Player - Adding money to non-zero bank"
-           (Project3110.Player.get_bank
-              (Project3110.Player.bank_money player2 5))
-           11;
-         create_test "Testing Player - Removing from non-zero bank"
-           (Project3110.Player.get_bank
-              (Project3110.Player.remove_from_bank player2 4))
-           2;
-         create_test "Testing - Drawing multiple cards from deck "
-           (List.length new_deck) 82;
-       ]
+  [
+    (******************** Deck.mli tests ********************************)
+    ("Deck test" >:: fun _ -> assert_equal (List.length test_deck) 89);
+    create_test "Testing - draw_card" (List.length new_deck) 82;
+    (******************** Player.mli tests ********************************)
+    (******************** init_player tests *****************************)
+    create_test "Testing Player - init name"
+      (Project3110.Player.get_name player1_init)
+      "player1";
+    create_test "Testing Player - init properties"
+      (Project3110.Player.get_properties player1_init)
+      [];
+    create_test "Testing Player - init bank: 0"
+      (Project3110.Player.get_bank player1_init)
+      0;
+    create_test "Testing Player - init deck: empty list"
+      (Project3110.Player.get_hand player1_init)
+      [];
+    (********************* add/remove_from_hand tests *******************)
+    create_test "Testing Player - add cards to hand "
+      (List.rev (Project3110.Player.get_hand player1))
+      [
+        Property ("Railroad", "Reading Railroad");
+        Property ("Railroad", "Pennsylvania Railroad");
+        Money 4;
+        Money 1;
+        Action "Debt Collector";
+      ];
+    create_test "Testing Player - remove card from empty hand"
+      (Project3110.Player.remove_from_hand player1_init (Money 5))
+      player1_init;
+    create_test "Testing Player - Remove card from hand that isn't there"
+      (List.rev
+         (Project3110.Player.get_hand
+            (Project3110.Player.remove_from_hand player1 (Money 5))))
+      [
+        Property ("Railroad", "Reading Railroad");
+        Property ("Railroad", "Pennsylvania Railroad");
+        Money 4;
+        Money 1;
+        Action "Debt Collector";
+      ];
+    create_test "Testing Player - removing a card from a player"
+      (List.rev
+         (Project3110.Player.get_hand
+            (Project3110.Player.remove_from_hand player1 (Money 4))))
+      [
+        Property ("Railroad", "Reading Railroad");
+        Property ("Railroad", "Pennsylvania Railroad");
+        Money 1;
+        Action "Debt Collector";
+      ];
+    (******** add/remove properites/ get_property_sets test *************)
+    create_test "Testing Player - 1 full set in properties"
+      (Project3110.Player.get_property_sets player1)
+      1;
+    (***************** bank_money/remove_from_bank tests *****************)
+    create_test "Testing Player - Removing money from 0 bank "
+      (Project3110.Player.get_bank player1)
+      0;
+    create_test "Testing Player - Adding money to non-zero bank"
+      (Project3110.Player.get_bank (Project3110.Player.bank_money player2 5))
+      11;
+    create_test "Testing Player - Removing from non-zero bank"
+      (Project3110.Player.get_bank
+         (Project3110.Player.remove_from_bank player2 4))
+      2;
+    create_test "Testing - Drawing multiple cards from deck "
+      (List.length new_deck) 82;
+  ]
 
-       let run_tests = "test suite" >::: List.flatten [ tests; card_action_tests ]
+let run_tests = "test suite" >::: List.flatten [ tests; card_action_tests ]
+let _ = run_test_tt_main run_tests
